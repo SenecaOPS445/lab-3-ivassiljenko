648cf18cccd6c6172cb52222696eaf426c5c27d3
Ivan Vassiljenko <ivassiljenko@myseneca.ca>
2024-05-31 16:06:37 -0400

    Individual message or note.

---
+++ lab3a.py
@@ -0,0 +1,23 @@
+
+#!/usr/bin/env python3
+
+
+
+# ID: ivassiljenko
+
+def return_text_value():
+    return 'Good Morning Terry'
+
+
+
+def return_number_value():
+
+    pass
+
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    print(return_number_value()
+
